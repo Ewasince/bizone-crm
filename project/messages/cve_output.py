352f2d9d0cee478b76a4f3d62270c4d75794759e
Vladislav Lavr <ewasince@gmail.com>
2023-05-17 21:04:33 +0300

    output fix

--- project/messages/cve_output.py
+++ project/messages/cve_output.py
@@ -150,6 +150,8 @@ class ParamsFormatter:
         return additional_sources
 
     def get_description(self, description):
+        description = description.replace('<', '\<')
+        description = description.replace('>', '\>')
         return self.__get_param_or_template(description)
 
     def get_pocs(self, pocs):
@@ -192,8 +194,6 @@ def get_cve_by_id_output_text(cve: Cve) -> str:
     formatter = ParamsFormatter()
 
     message = f"""
-<b>По данному cve id найдена информация</b>:
-
 ① <b>Номер CVE</b>: <a href='{formatter.get_link(cve.id)}'>{formatter.get_id(cve.id)}</a>
 ② <b>Дата/время регистрации CVE</b>: {formatter.get_date(cve.date)}
 ⑤ <b>Уровень критичности CVSS v2</b>: {formatter.get_cvss2_and_score(cve.cvss2, cve.score_v2)}
@@ -207,6 +207,8 @@ def get_cve_by_id_output_text(cve: Cve) -> str:
 ⑫ <b>Необходимые действия по устранению уязвимости</b>: {formatter.get_eliminations(cve.elimination)}
 ⑬ <b>POC</b>: {formatter.get_pocs(cve.poc)}
     """
+
+
 
     return message
 
